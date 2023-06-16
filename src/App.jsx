44c5ae3e6629c6621d86749547b8799667df1fcc
ZiyaOzgul <ziya.d.ozgul@gmail.com>
2023-06-16 15:53:32 +0300

    Responsive Design

--- src/App.jsx
+++ src/App.jsx
@@ -19,19 +19,19 @@ function App() {
   }, [dispatch]);
   const page = currentPage - 1;
   return (
-    <div className="w-full h-screen flex  items-center justify-start bg-gradient-to-br from-yellow-100 to-amber-200">
-      <nav className="w-3/5 h-screen flex items-center justify-start space-y-6 flex-col  ">
-        <h1 className="font-bold text-6xl font-piksel mb-24">
+    <div className="w-full h-screen flex flex-col-reverse lg:flex-row items-center justify-start bg-gradient-to-br from-yellow-100 to-amber-200">
+      <nav className="w-full h-2/5 lg:w-3/5 lg:h-screen flex items-center justify-start space-y-6 flex-col  ">
+        <h1 className="font-medium text-lg lg:font-bold lg:text-6xl font-piksel my-6 mb-0 lg:my-0 lg:mb-24">
           Select A Pokemon
         </h1>
         {allPokemons == "" ? (
           <h1 className="font-bold text-6xl font-piksel"> Loading ...</h1>
         ) : (
-          <div className="w-full h-auto  overflow-x-hidden grid grid-cols-3 gap-4 px-4">
+          <div className="w-full h-auto  overflow-x-hidden grid grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4 px-2 lg:px-4">
             {allPokemons.map((item, key) => (
               <Link to={`/pokemon/${key}`} key={key}>
                 <div
-                  className="font-piksel flex items-center justify-evenly space-x-4 w-80 h-20 rounded-2xl bg-sky-400 border border-black group hover:bg-sky-300 ease-in-out duration-500"
+                  className="font-piksel flex items-center justify-start lg:justify-evenly space-x-1 lg:space-x-4 w-48 h-10 lg:w-80 lg:h-20 rounded-lg lg:rounded-2xl bg-sky-400 border border-black group hover:bg-sky-300 ease-in-out duration-500"
                   onClick={() => {
                     dispatch(
                       fetchCurrentPokemonAsync({
@@ -40,30 +40,34 @@ function App() {
                     );
                   }}
                 >
-                  <p className="font-medium text-sm px-2 py-2">{key + 1}</p>
+                  <p className="font-normal text-xs lg:font-medium lg:text-sm px-1 py-1 lg:px-2 lg:py-2">
+                    {key + 1}
+                  </p>
                   <img
                     src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
                       key + 1
                     }.png`}
                     alt=""
-                    className="group-hover:scale-105 ease-in-out duration-500"
+                    className="h-10 w-10 lg:w-auto lg:h-auto group-hover:scale-105 ease-in-out duration-500"
                   />
-                  <p className="font-medium text-base group-hover:-translate-x-2 ease-in-out duration-500">
+                  <p className="font-light text-sm lg:font-medium  lg:text-base group-hover:-translate-x-1 lg:group-hover:-translate-x-2 transition-all ease-in-out duration-500">
                     {item.name}
                   </p>
                 </div>
               </Link>
             ))}
-            <p
-              className="flex items-center justify-center font-piksel cursor-pointer hover:scale-105 duration-500 ease-in-out "
-              onClick={() => dispatch(fetchPokemonsAsync(currentPage))}
-            >
-              Load More ...
-            </p>
+            <div className="flex items-center justify-center w-full">
+              <p
+                className="font-piksel  cursor-pointer hover:outline hover:scale-105 transition-all duration-500 ease-in-out "
+                onClick={() => dispatch(fetchPokemonsAsync(currentPage))}
+              >
+                Load More ...
+              </p>
+            </div>
           </div>
         )}
       </nav>
-      <div className="w-2/5 h-screen bg-gradient-to-b from-yellow-500 to-amber-400 shadow-xl overflow-hidden">
+      <div className="w-full h-3/5 lg:w-2/5 lg:h-screen bg-gradient-to-b from-yellow-500 to-amber-400 shadow-xl overflow-hidden">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/pokemon/:pokeid" element={<PokemonPage />} />
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -14,46 +14,61 @@ const Pokemon = () => {
 
   if (isLoading) {
     <div className="flex items-center justify-center">
-      <h1 className="font-semibold text-4xl font-piksel">Loading ...</h1>
+      <h1 className="font-medium lg:font-semibold text-xl lg:text-4xl font-piksel">
+        Loading ...
+      </h1>
     </div>;
   } else {
     return (
-      <div className="w-full h-full flex flex-col items-center justify-center spcae-y-4 relative">
+      <div className="w-full h-full flex flex-col items-center justify-center  space-y-1 lg:spcae-y-4 relative">
         <img
           src={`${pokemon.sprites.other.dream_world.front_default}`}
           alt={`${pokemon.name}`}
-          className="w-40 h-60"
+          className="h-32 w-28 lg:w-40 lg:h-60"
         />
-        <h1 className="font-semibold text-xl font-piksel">
+        <h1 className="font-medium text-base lg:font-semibold lg:text-xl font-piksel">
           Name : {pokemon.forms[0].name}
         </h1>
-        <div className="w-full flex items-center justify-center space-x-6 mt-6">
-          <h1 className="py-2 font-semibold text-lg font-piksel">Types : </h1>
+        <div className="w-full flex items-center justify-center space-x-1 lg:space-x-6 mt-1 lg:mt-6">
+          <h1 className="py-2 font-medium lg:font-semibold text-base lg:text-lg font-piksel">
+            Types :
+          </h1>
           {pokemon.types.map((item, key) => (
-            <h1 key={key} className="font-medium text-xl font-piksel">
+            <h1
+              key={key}
+              className="font-normal lg:font-medium text-lg lg:text-xl font-piksel"
+            >
               {item.type.name}
             </h1>
           ))}
         </div>
-        <div className="w-full flex items-center justify-center space-x-6 mt-6">
-          <h1 className="py-2 font-semibold text-lg font-piksel">
+        <div className="w-full flex items-center justify-center space-x-1 lg:space-x-6 mt-1 lg:mt-6">
+          <h1 className="py-2 font-medium lg:font-semibold text-base lg:text-lg font-piksel">
             Abilities :
           </h1>
           {pokemon.abilities.map((item, key) => (
-            <h1 key={key} className="font-medium text-xl font-piksel">
+            <h1
+              key={key}
+              className="font-normal lg:font-medium text-lg lg:text-xl font-piksel"
+            >
               {item.ability.name}
             </h1>
           ))}
         </div>
-        <div className="w-full flex flex-col items-center justify-center space-y-4 mt-6">
-          <h1 className="font-medium text-xl font-piksel">Base Stats :</h1>
-          <div className="w-full flex items-center justify-center space-x-6 mt-6 flex-wrap">
+        <div className="w-full flex flex-col items-center justify-center space-y-1 lg:space-y-4 mt-1 lg:mt-6">
+          <h1 className="font-normal lg:font-medium text-lg lg:text-xl font-piksel">
+            Base Stats :
+          </h1>
+          <div className="w-full flex items-center justify-center space-x-1 lg:space-x-6 mt-1 lg:mt-6 flex-wrap">
             {pokemon.stats.map((item, key) => (
               <div className="flex items-center justify-start">
-                <h1 className="font-semibold text-lg font-piksel" key={key}>
-                  {item.stat.name} :{" "}
+                <h1
+                  className="font-medium lg:font-semibold text-base lg:text-lg font-piksel"
+                  key={key}
+                >
+                  {item.stat.name} :
                 </h1>
-                <p className="font-medium text-xl font-piksel">
+                <p className="font-normal lg:font-medium text-lg lg:text-xl font-piksel">
                   {item.base_stat}
                 </p>
               </div>
@@ -61,17 +76,19 @@ const Pokemon = () => {
           </div>
         </div>
         <Link to={"/"}>
-          <h1 className="absolute top-0 left-20 px-4 font-semibold text-4xl font-pokemon text-red-600 hover:scael-110 ease-in-out duration-500">
+          <h1 className="absolute top-0 left-4 md:left-20 px-1 md:px-4 font-medium md:font-semibold text-xl md:text-4xl font-pokemon text-red-600 hover:scael-110 ease-in-out duration-500">
             PoKeMoN
           </h1>
         </Link>
         <Link to={"/"}>
           <div className="absolute right-2 top-3 flex items-center justify-center group">
             <UilAngleLeftB className="group-hover:-translate-x-4 ease-in-out duration-500"></UilAngleLeftB>
-            <h1 className="font-semibold text-xl font-piksel">Back To Home</h1>
+            <h1 className="font-medium lg:font-semibold text-lg lg:text-xl font-piksel">
+              Back <i className="hidden lg:inline-block">To Home</i>
+            </h1>
           </div>
         </Link>
-        <div className="flex items-center justify-evenly space-x-4 absolute bottom-1 right-4">
+        <div className="flex items-center justify-evenly space-x-1 lg:space-x-4 absolute bottom-1 right-4">
           <a href="https://github.com/ZiyaOzgul" target="_blank">
             <UilGithub className="w-10 h-10 font-piksel text-gray-600 hover:scale-110 ease-in-out duration-500"></UilGithub>
           </a>
